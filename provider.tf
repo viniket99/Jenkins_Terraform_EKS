649b790e0308333f60ca223b2fd2258ecece1b45
viniket99 <viniketbulle99@gmail.com>
2025-09-22 10:05:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ locals {
   region = "eu-north-1"
   name = "vini-eks-cluster"
   vpc_cidr = "10.0.0.0/16"
-  azs = ["eu-north-1b", "eu-north-1c"]
+  azs = ["ap-south-1b", "ap-south-1c"]
   public_subnet = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet = ["10.0.3.0/24", "10.0.4.0/24"]
   intra_subnet = ["10.0.5.0/24", "10.0.6.0/24"]
